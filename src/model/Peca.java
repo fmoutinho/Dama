93321f436ed5d986b14fd523bceaa702b14950c9
Fernando Moutinho <fernandomvmoutinho@gmail.com>
2015-04-28 02:10:31 -0300

    Arrumação de acordo com a norma do Java;
    package model
    Classe Peca dentro de model

--- src/model/Peca.java
+++ src/model/Peca.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dama;
+package model;
 
 import JPlay.Sprite;
 import java.awt.Point;
