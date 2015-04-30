7772b4858cd4de4446a8cb940dbc94f12abbbb5a
lfnascimento <nascimento.lsfo@gmail.com>
2015-04-29 23:41:02 -0300

    Codigo da classe Casa mais limpo : )

--- src/dama/Casa.java
+++ src/dama/Casa.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dama;
 
 import JPlay.GameImage;
 
-/**
- *
- * @author fernando
- */
 public class Casa {
     GameImage imagem;
     public Casa (String imagem) {
