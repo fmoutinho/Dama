2b24ce13fbb5c55bae2f298b1b28b1450361ebd1
Fernando Moutinho <fernandomvmoutinho@gmail.com>
2015-04-30 01:15:34 -0300

    Mudanças:
    Tabuleiro no background,
    classes Casa e TabuleiroLuis no pacote correto.

--- src/dama/Dama.java
+++ src/dama/Dama.java
@@ -10,37 +10,35 @@ import JPlay.GameImage;
 import JPlay.Keyboard;
 import JPlay.Mouse;
 import JPlay.Window;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Dama {
-    
+
     public static void main(String[] args) {
-        
-        Window janela = new Window(640, 640);     
-        Peca peca1 = new Peca(88, 8);             
-        
-        TabuleiroLuis tabuleiro = new TabuleiroLuis();
-        
+
+        Window janela = new Window(640, 640);
+        Peca peca1 = new Peca(88, 8);
+
+        GameImage imagemFundo = new GameImage("tabuleiro.jpg");
         Mouse mouse = janela.getMouse();
         Keyboard teclado = janela.getKeyboard();
-        
+
         boolean executando = true;
-        
+
         while (executando) {
-           
+
+            imagemFundo.draw();
             peca1.draw();
             janela.display();
-            
-            if (mouse.isLeftButtonPressed() == true)
-               peca1.andar(mouse.getPosition());    
-            
-            if(teclado.keyDown(Keyboard.ESCAPE_KEY) == true)
+
+            if (mouse.isLeftButtonPressed() == true) {
+                peca1.andar(mouse.getPosition());
+            }
+
+            if (teclado.keyDown(Keyboard.ESCAPE_KEY) == true) {
                 executando = false;
+            }
         }
         janela.exit();
     }
- 
-} 
+
+}
